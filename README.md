d77357ef938d2ec1497ba87222a647b2e5a0a9aa
TomL384 <350286454@tdsb.ca>
2026-05-07 00:06:23 +0000

    Final

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Names and ages are parallel arrays.  Return the age associated with the name.
 
 2. Create a function called _createLengthArray()_, which take in a String array.
 Return an integer array with the length of each of the strings.
-
-3. Create a function called _swapPeople()_, which take in a String Array (names), integer array (ages), String (name), String(name).
+_swapPeople
+3. Create a function called ()_, which take in a String Array (names), integer array (ages), String (name), String(name).
 Swap the two people and their ages in each array.
--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,52 @@ public class PracticeProblem {
 
 	}
 
+	public static int getAge(String[] names, int[] ages, String name){
+		int count = 0;
+		for (int i = 0; i < names.length; i ++){
+			if (names[i].equals(name)){
+				count = i;
+				return ages[count];
+			}
+		}
+		return ages[count];
+	}
+
+	public static int[] createLengthArray(String[] arr){
+		int count = 0;
+		int[] arr1 = new int[arr.length];
+		for (int i = 0; i < arr.length; i ++){
+			count = arr[i].length();
+			arr1[i] = count;
+				
+		}
+		return arr1;
+	}
+
+	public static void swapPeople(String[] names, int[] ages, String name, String name1){
+		int s1 = 0;
+		int s2 = 0;
+		int count = 0;
+		int count1 = 0;
+		for (int i = 0; i < names.length; i++){
+			if (names[i].equals(name)&&count==0){
+				s1 = i;
+				count ++;
+			}
+			if (names[i].equals(name1)&&count1==0){
+				s2 = i;
+				count1 ++;
+			}
+		}
+		String swap1a = names[s2];
+		String swap1b = names[s1];
+		int swap2a = ages[s2];
+		int swap2b = ages[s1];
+		names[s1] = swap1a;
+		names[s2] = swap1b;
+		ages[s1] = swap2a;
+		ages[s2] = swap2b;
+	}
+		
 }
+
